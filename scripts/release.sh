2ec7cb0e352c5afa27cbfcd246243e1042de4250
namtranase <trannam.ase@gmail.com>
2024-03-18 10:40:04 +0700

    update: release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+# Remove previous distribution files
 rm -rf dist/*
+
+# Build the distribution
 python3 setup.py sdist bdist_wheel
-delocate-wheel -w fixed_wheels -v dist/*.whl
+
+# Check the operating system
+OS="$(uname)"
+if [ "$OS" = "Darwin" ]; then
+    # macOS specific commands
+    delocate-wheel -w fixed_wheels -v dist/*.whl
+    mv fixed_wheels/*.whl dist/
+elif [ "$OS" = "Linux" ]; then
+    # Linux specific commands (if any can be added here)
+    echo "Linux OS detected. No additional steps required for Linux."
+fi
+
+# Upload the distribution to PyPI
+twine upload dist/*
